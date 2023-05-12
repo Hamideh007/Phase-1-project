8f680b8a13b0ad848da7863698edbb792d78794d
hamideh rahmani <rahmani.mahtab@gmail.com>
2023-05-12 05:06:29 -0400

    fixed social media amd logo

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
-
 // event listeners
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
     mealDetailsContent.parentElement.classList.remove('showRecipe');
 });
-
 
 // get meal list that matches with the ingredients
 function getMealList(){
@@ -41,7 +39,6 @@ function getMealList(){
         mealList.innerHTML = html;
     });
 }
-
 
 // get recipe of the meal
 function getMealRecipe(e){
